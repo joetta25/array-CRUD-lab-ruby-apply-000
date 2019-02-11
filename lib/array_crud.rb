95c50d4635ac076548d0aab5840745fa0c04b49f
joetta Hall <joetta.chasidy@gmail.com>
2019-02-11 16:28:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array.push(5)
+array_element.push("horse")
 end
 
 def add_element_to_start_of_array(array, element)
